705445c14cdd17bf488e0d8ec204f8f843f7d4f3
venkatp57 <venkat.pusala@gmail.com>
2022-11-24 15:13:40 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './functionBased/components/TodoContainer';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import About from "./functionBased/pages/About";
 import NotMatch from "./functionBased/pages/NotMatch";
 import Layout from './functionBased/pages/Layout';
